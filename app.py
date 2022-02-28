5883630cf5c9efea6a677156e88aeef2d12ad383
Mohammadreza <vatankhah@yahoo.com>
2022-02-28 17:18:55 +0400

    initial app

---
+++ app.py
@@ -0,0 +1,18 @@
+import requests
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.get("/github")
+async def github():
+    response = requests.get(
+        "https://api.github.com/repos/Mohammadreza-v/flask_log/pulls"
+    )
+
+    return jsonify(response.json())
+
+
+@app.get("/gitlab")
+async def gitlab():
+    return ""
